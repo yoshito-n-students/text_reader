b6e05af0e6d37989322ba9e23c483f2680c023f1
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-05-22 16:22:22 +0900

    additionally download text detector data

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-# assuming this script locates a directory for dictnet data
+# assuming this script locates a directory for dnn data
 DATA_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
 
+# text box detector data
+wget https://github.com/opencv/opencv_contrib/raw/master/modules/text/samples/textbox.prototxt -P ${DATA_DIR}
+wget https://www.dropbox.com/s/g8pjzv2de9gty8g/TextBoxes_icdar13.caffemodel?dl=0 -O ${DATA_DIR}/TextBoxes_icdar13.caffemodel
+
+# word recognizer data
 wget http://nicolaou.homouniversalis.org/assets/vgg_text/dictnet_vgg.caffemodel -P ${DATA_DIR}
 wget http://nicolaou.homouniversalis.org/assets/vgg_text/dictnet_vgg_deploy.prototxt -P ${DATA_DIR}
 wget http://nicolaou.homouniversalis.org/assets/vgg_text/dictnet_vgg_labels.txt -P ${DATA_DIR}
